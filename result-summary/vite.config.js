b0bf3ba1a76ce7a2ce48425ec4b7f9b6438b9d99
Victor Ike <victorike08@gmail.com>
2023-05-30 22:31:19 -0500

    update

--- result-summary/vite.config.js
+++ result-summary/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/result-summary-component',
+  base: '/result-summary-component/',
   plugins: [react()],
 });
