b8af77ec5c01a81dcde74eb0f92ae4684609c336
Victor Ike <victorike08@gmail.com>
2023-05-30 21:54:15 -0500

    Create Component

---
+++ result-summary/src/components/Display.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+
+export default function Display(props) {
+  let secondaryClassName;
+  if (props.category === 'Reaction') {
+    secondaryClassName = 'card-red';
+  } else if (props.category === 'Memory') {
+    secondaryClassName = 'card-yellow';
+  } else if (props.category === 'Verbal') {
+    secondaryClassName = 'card-green';
+  } else {
+    secondaryClassName = 'card-blue';
+  }
+  return (
+    <>
+      <div className="cards">
+        <div className={`card ${secondaryClassName}`}>
+          <div className="left-col">
+            <img src={`${props.icon}`} alt="Reaction Icon" />
+            <span>{props.category}</span>
+          </div>
+          <div className="right-col">
+            <p>
+              {props.score} <span>/100</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
